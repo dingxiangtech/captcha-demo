b1795d5ce32100f6ed7cac8011ac484e16d60017
DingXiang <github@dingxiang-inc.com>
2023-04-04 17:11:32 +0800

    Update CaptchaService.java

--- captchaJavaDemo/src/main/java/com/dingxianginc/captcha/demo/service/CaptchaService.java
+++ captchaJavaDemo/src/main/java/com/dingxianginc/captcha/demo/service/CaptchaService.java
@@ -19,16 +19,15 @@ public class CaptchaService {
          * ip 可选，提交业务参数的客户端ip
          **/
         try {
-            String appId = "appId";         // 顶象控制台应用管理或应用配置模块获取
-            String appSecret = "appSecret";      // 顶象控制台应用管理或应用配置模块获取
-            CaptchaClient captchaClient = new CaptchaClient(appId,appSecret);
+            String appId = "appId";            // 顶象控制台应用管理或应用配置模块获取
+            String appSecret = "appSecret";    // 顶象控制台应用管理或应用配置模块获取
+            CaptchaClient captchaClient = new CaptchaClient(appId,appSecret);            
+            // 指定服务器地址，请登录顶象防御云控制台，访问验证码的应用管理，在页面最上方获取apiServer值
             captchaClient.setCaptchaUrl("https://cap.dingxiang-inc.com/api/tokenVerify");
-            //指定服务器地址，saas可在控制台，应用管理页面最上方获取
+            
             CaptchaResponse response = captchaClient.verifyToken(token);
-            //针对一些token冒用的情况，业务方可以采集客户端ip随token一起提交到验证码服务，验证码服务除了判断token的合法性还会校验提交业务参数的客户端ip和验证码颁发token的客户端ip是否一致
             System.out.println(response.getCaptchaStatus());
-            //确保验证状态是SERVER_SUCCESS，SDK中有容错机制，在网络出现异常的情况会返回通过
-            //验证码服务采集到的客户端ip
+            // 确保验证状态是SERVER_SUCCESS，SDK中有容错机制，在网络出现异常的情况会返回通过
             if (response.getResult()) {
                 /**token验证通过，继续其他流程**/
                 res = true;
